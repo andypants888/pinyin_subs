9d185e54a229ca5f6ca2f99e2b74ba8ff42027cf
andypants <79945330+andypants888@users.noreply.github.com>
2025-02-25 20:47:12 -0800

    update file reading

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
+def main(input_srt):
+    with open(input_srt, "r") as f:
+        content = f.read()
+
+    print(content)
+
     print("Hello from pinyinsubs!")
 
 
+
 if __name__ == "__main__":
-    main()
+    main("resources/How.To.S01E01._en_tiny.srt")
